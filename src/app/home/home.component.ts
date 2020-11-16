ba489af5ea32678c28e143117bc8d382f0b917f8
Aradhana Singh <aradhana971127@gmail.com>
2020-11-16 16:36:29 +0530

    commit 1

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    let response = this.http.get<any>("http://localhost:8080/");
+    console.log(response);
+    response.subscribe((data)=>console.log(data));
+  }
+}
