0fcdcca2b059c547ba0b6af48862aeae8d3928a4
Aradhana Singh <aradhana971127@gmail.com>
2020-11-26 16:58:19 +0530

    cnflicts resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -76,14 +76,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatTreeModule} from '@angular/material/tree';
 import {OverlayModule} from '@angular/cdk/overlay';
-<<<<<<< HEAD
 import {SplitButtonModule} from 'primeng/splitbutton';
 import {RouterModule} from '@angular/router';
 import { ToastModule } from 'primeng/toast';
-=======
 import {TieredMenuModule} from 'primeng/tieredmenu';
 
->>>>>>> 90b8d4bc9aad886a59c66f281745cd74484ea179
 // import { BasicScrollComponent } from './basic-scroll/basic-scroll.component';
 // import { InfiniteScrollComponent } from './infinite-scroll/infinite-scroll.component';
 
@@ -165,15 +162,12 @@ import {TieredMenuModule} from 'primeng/tieredmenu';
     MatTreeModule,
     OverlayModule,
     PortalModule,
-<<<<<<< HEAD
     RouterModule,
     ScrollingModule,
     SplitButtonModule,
-    ToastModule 
-=======
+    ToastModule,
     ScrollingModule,
     TieredMenuModule
->>>>>>> 90b8d4bc9aad886a59c66f281745cd74484ea179
   ],
   providers: [ CookieService, AuthService, AuthGuard, {
     provide : HTTP_INTERCEPTORS,
